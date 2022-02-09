c86697fc2ed4b3e2fabc7bf473c08001363c8e1e
Héctor González Pereda <51021644+Fidesone@users.noreply.github.com>
2022-02-09 19:17:57 +0100

    initial commit

---
+++ src/HelloWord.java
@@ -0,0 +1,9 @@
+public class HelloWord {
+    public static void main(String [] args ){
+        // Definicion
+        String palabra = "Hello World";
+        int numero = 25;
+        System.out.println(palabra);
+        System.out.println(numero);
+    }
+}
