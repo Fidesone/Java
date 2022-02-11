f4199cc60e6b92bdf2bba9bf454b2caff9cac3ff
Héctor González Pereda <51021644+Fidesone@users.noreply.github.com>
2022-02-11 11:19:18 +0100

    switch statement

--- src/ControlStatements.java
+++ src/ControlStatements.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class ControlStatements {
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         // Sentencias de control: IF- ELSE
         System.out.println("En que mes estamos? (del 1 al 12)");
         Scanner consola = new Scanner(System.in);
@@ -10,23 +10,49 @@ public class ControlStatements {
 
         if (mes == 1 || mes == 2 || mes == 12) {
             estacion = "Invierno";
-            System.out.println( "Es el mes " + mes + " , asi que nos encontramos en "5 + estacion);
+            System.out.println("Es el mes " + mes + " , asi que nos encontramos en " + estacion);
 
 
         } else if (mes == 3 || mes == 4 || mes == 5) {
             estacion = "Primavera";
-            System.out.println( "Es el mes " + mes + " , asi que nos encontramos en " + estacion);
+            System.out.println("Es el mes " + mes + " , asi que nos encontramos en " + estacion);
 
         } else if (mes == 6 || mes == 7 || mes == 8) {
             estacion = "Verano";
-            System.out.println( "Es el mes " + mes + " , asi que nos encontramos en " + estacion);
+            System.out.println("Es el mes " + mes + " , asi que nos encontramos en " + estacion);
 
         } else if (mes == 9 || mes == 10 || mes == 11) {
             estacion = "Otonio";
-            System.out.println( "Es el mes " + mes + " , asi que nos encontramos en " + estacion);
+            System.out.println("Es el mes " + mes + " , asi que nos encontramos en " + estacion);
 
-            // Sentencias de control: IF- ELSE
         }
+
+
+    }
+}*/
+
+    public static void main(String[] args) {
+        // Sentencias de control: IF- ELSE
+        System.out.println("En que mes estamos? (del 1 al 12)");
+        Scanner consola = new Scanner(System.in);
+        var mes = Integer.parseInt(consola.nextLine());
+        var estacion = "Estacion desconocida";
+
+        switch (mes){
+            case 1: case 2: case 12:
+                estacion = "Invierno";
+                break;
+            case 3: case 4: case 5:
+                estacion = "Primavera";
+                break;
+            case 6: case 7: case 8:
+                estacion = "Verano";
+                break;
+            case 9: case 10: case 11:
+                estacion = "Otonio";
+                break;
+        }
+        System.out.println("Es el mes " + mes + " , asi que nos encontramos en " + estacion);
 
     }
 }
